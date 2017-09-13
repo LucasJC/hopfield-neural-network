4c180669c13db046a39a5addf004f3262270561d
Lucas J. Couchot <lucas.couchot@gmail.com>
2017-09-13 02:17:47 -0300

    configuro gradle y genero pirmer ejemplo de una red Hopfield

---
+++ src/main/java/ar/edu/utn/frba/ia/hopfield/example/Main.java
@@ -0,0 +1,35 @@
+package ar.edu.utn.frba.ia.hopfield.example;
+
+import java.util.Arrays;
+
+import org.neuroph.core.data.DataSet;
+import org.neuroph.core.data.DataSetRow;
+import org.neuroph.nnet.Hopfield;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		//set de entrenamiento
+		DataSet trainingSet = new DataSet(9);
+		trainingSet.add(new DataSetRow(new double[]{1,0,1, 1, 1, 1, 1, 0, 1}));
+		trainingSet.add(new DataSetRow(new double[]{1, 1, 1, 0, 1, 0, 0, 1, 0}));
+		
+		//creo red con 9 neuronas
+		Hopfield network = new Hopfield(9);
+		network.learn(trainingSet);
+		
+		DataSet testSet = new DataSet(9);
+		testSet.add(new DataSetRow(new double[]{1, 0, 0, 1, 0, 1, 1, 0, 1}));
+		testSet.add(new DataSetRow(new double[]{1, 1, 1, 0, 1, 0, 0, 1, 0}));
+		testSet.add(new DataSetRow(new double[]{1, 0, 1, 0, 1, 0, 0, 1, 0}));
+		
+		for(DataSetRow row : testSet.getRows()) {
+			network.setInput(row.getInput());
+			network.calculate();
+			network.calculate();
+			System.out.println("Resultado:");
+			System.out.println("Input: " + Arrays.toString(row.getInput()));
+			System.out.println("Output: " + Arrays.toString(network.getOutput()));
+		}
+	}
+}
